48c354ca479e2db5707bd8cf8a6ce70ce99b6320
santiagoprietooo <santiago.prieto@davinci.edu.ar>
2024-12-08 00:16:53 -0300

    logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Menu from "./Menu";
-import Logo from "/Icons/river-plate-badge.png";
+import Logo from "/public/Icons/river-plate-badge.png";
 import { NavLink } from "react-router-dom";
 
 function Header() {
